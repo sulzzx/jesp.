e796fb743012cf97a9c8ed427f9700f8542419d7
jesp <132315720+jesphery@users.noreply.github.com>
2024-06-19 19:50:30 -0300

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".multiple-text", {
-    strings: ["Frontend Developer", "Backend Developer", "Youtuber", "Brazillian"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-}) 
