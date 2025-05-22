b222a6e80751fc0ce9178d8a5f684ab97f4f207e
sulz. <132315720+sulzzx@users.noreply.github.com>
2025-05-22 20:02:50 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text", {
-    strings: ["Frontend Developer", "Backend Developer", "Youtuber", "Brazillian"],
+    strings: ["Frontend Developer", "Data Analyst", "Youtuber", "Brazillian"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
