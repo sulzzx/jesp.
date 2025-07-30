c7a46655314dd1f18a0f59cd9b00838a98d09b47
sulz. <132315720+sulzzx@users.noreply.github.com>
2025-07-29 22:12:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ var typed = new Typed(".multiple-text", {
     backDelay: 1000,
     loop: true
 }) 
+
+function mostrarLista() {
+  const lista = document.getElementById("minhaLista");
+  lista.classList.toggle("mostrar");
+}
