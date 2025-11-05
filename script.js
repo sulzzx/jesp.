a928a21c99b19519b2aaebd1a5ec3e949a135f9d
sulz. <132315720+sulzzx@users.noreply.github.com>
2025-11-05 16:57:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text", {
-    strings: ["Frontend Developer", "Analista de Dados", "Youtuber", "Brasileiro"],
+    strings: ["Analista de Dados", "Full-Stack Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
